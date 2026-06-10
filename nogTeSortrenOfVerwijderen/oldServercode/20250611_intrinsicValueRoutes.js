a36cf14e274f5e36672c6bd3d0955ba94cad770c
ArneVanRiel <arne.van.riel@hotmail.be>
2026-06-10 12:03:27 +0200

    Ruim repository op: verwijder genegeerde bestanden

--- nogTeSortrenOfVerwijderen/oldServercode/20250611_intrinsicValueRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/intrinsicValueRoutes.js
-const express = require('express');
-const router = express.Router();
-const { calculateIntrinsicValueRoute } = require('../controllers/intrinsicValueController');
-
-router.post('/calculate-intrinsic-value', calculateIntrinsicValueRoute);
-
-module.exports = router;
